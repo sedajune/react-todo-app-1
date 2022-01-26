80f45e2504b7fab82e364ae6c0f7352adc4dd17c
Gregor Adams <greg@pixelass.com>
2022-01-26 09:44:24 +0100

    feat: add todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,72 @@
-import React from "react";
-import Button from "../src/components/Button";
-
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
 const App = () => {
-  return (
-    <div>
-      <h1>Hello test</h1>
-      <Button>Click me!</Button>
-    </div>
-  );
+	const [value, setValue] = useState("");
+	const [todos, setTodos] = useState([
+		{
+			name: "Buy milk",
+			isChecked: true,
+			id: uuid(),
+		},
+	]);
+	return (
+		<div>
+			<h1>{value}</h1>
+			<form
+				onSubmit={event_ => {
+					event_.preventDefault();
+					setTodos([...todos, { name: value, isChecked: false, id: uuid() }]);
+					setValue("");
+				}}
+			>
+				<input
+					type="text"
+					value={value}
+					onChange={event_ => {
+						setValue(event_.target.value);
+					}}
+				/>
+				<button disabled={!value} type="submit">
+					Add
+				</button>
+			</form>
+			<ul>
+				{todos.map((todo, index) => {
+					return (
+						<li key={todo.id}>
+							<label>
+								<input
+									type="checkbox"
+									checked={todo.isChecked}
+									onChange={() => {
+										const update = [...todos];
+										update[index].isChecked = !update[index].isChecked;
+										setTodos(update);
+									}}
+								/>
+								<span
+									style={{
+										textDecoration: todo.isChecked ? "line-through" : "none",
+									}}
+								>
+									{todo.name}
+								</span>
+							</label>
+							<button
+								onClick={() => {
+									const update = [...todos];
+									update.splice(index, 1);
+									setTodos(update);
+								}}
+							>
+								Delete
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default App;
